ca4db9751b8891d1e0b3298e44259747e2b86c44
zhangpengxiang <352606194@qq.com>
2020-02-25 12:03:28 +0800

    项目提交

---
+++ src/test/java/com/test/word/WordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
